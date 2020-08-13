9593917715a2dfe0af9b1fe2ac95e0fb27bcd7f1
Victor Augusto <vdbernardes@gmail.com>
2020-08-13 19:27:01 -0300

    Aula 04

---
+++ Aula-04/exemplo-if.js
@@ -0,0 +1,17 @@
+let cor = "verde"
+
+if (cor === "verde"){
+    console.log("siga");
+
+} else if(cor === "amarelo"){
+    console.log("atenção");
+
+} else if(cor === "vermelho"){
+    console.log("pare");
+} else {
+    console.log("Favor Escolher entre verde, amarelo e vermelho");
+}
+
+
+
+
