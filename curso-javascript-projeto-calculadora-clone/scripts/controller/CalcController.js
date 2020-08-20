28a2dfb801aeda1a51990620ae13ab896eb8f919
Victor Augusto <vdbernardes@gmail.com>
2020-08-20 11:56:02 -0300

    Alteração Projeto Calculadora
    -Mudança nos nomes em português para nomes em inglês.

--- curso-javascript-projeto-calculadora-clone/scripts/controller/CalcController.js
+++ curso-javascript-projeto-calculadora-clone/scripts/controller/CalcController.js
@@ -3,7 +3,7 @@ class CalcController {
     constructor(){
 
         this._displayCalc = "0";
-        this._dataAtual;
+        this._currentDate;
     }
 
     get displayCalc(){
@@ -16,10 +16,10 @@ class CalcController {
     }
 
     get dataAtual(){
-        return this._dataAtual;
+        return this._currentDate;
     }
 
     set dataAtual(){
-        this._dataAtual;
+        this._currentDate;
     }
 }
