750c213129d93ef5f8850c840be3a31ac6c12f9e
Victor Augusto <vdbernardes@gmail.com>
2020-09-14 10:41:26 -0300

    querySelector das caixa de texto
    -Feitos os query selector das caixas de texto.
    -Alterados ids dos campos email e password.
    -Adicionado id para administrador.

--- curso-javascript-projeto-usuarios/index.js
+++ curso-javascript-projeto-usuarios/index.js
@@ -1,1 +1,11 @@
-alert("Hello World");
+var name = document.querySelector("#exampleInputBirth");
+var gender = document.querySelectorAll("#form-user-create [name = gender]:checked");
+var birth = document.querySelector("#exampleInputName");
+var country = document.querySelector("#exampleInputCountry");
+var email = document.querySelector("#exampleInputEmail");
+var password = document.querySelector("#exampleInputPassword");
+var photo = document.querySelector("#exampleInputFile");
+var admin = document.querySelector("#exampleInputAdmin");
+
+
+
